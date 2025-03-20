137c40ed740bcd59a2635f9f4cf6881494a4484d
Leibril2007 <ariabi.sr@gmail.com>
2025-03-20 08:51:39 -0600

    diseño del login terminado

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import { cargarLogin } from "./components/login/login.js";
+
 let DOM = document.querySelector("#root");
 
 function cargarDOM(){
 
 }
+
+DOM.appendChild(cargarLogin());
+
+cargarDOM();
